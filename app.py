beab8ce785b22a77b367822d81a79f240e69499e
KiaraCodeWorld <128760279+KiaraCodeWorld@users.noreply.github.com>
2024-01-21 14:52:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,16 +6,7 @@ import json
 API_TOKEN = os.environ["API_TOKEN"]
 API_URL =  "https://api-inference.huggingface.co/models/deepset/roberta-base-squad2" #"https://api-inference.huggingface.co/models/ESGBERT/EnvironmentalBERT-environmental" #Add a URL for a model of your choosing
 headers = {"Authorization": f"Bearer {API_TOKEN}"}
-
-def query(prompt_question, prompt_context, api_url=API_URL, headers=headers):
-    prompt = {
-        "question": prompt_question,
-        "context": prompt_context,
-    }
-
-    payload = {
-        "inputs": prompt,
-        "parameters": { #Try and experiment with the parameters
+default_parms = { #Try and experiment with the parameters
             "max_new_tokens": 1024,
             "temperature": 0.9,
             "top_p": 0.9,
@@ -23,6 +14,16 @@ def query(prompt_question, prompt_context, api_url=API_URL, headers=headers):
             "return_full_text": False,
             "wait_for_model": True,
         }
+
+def query(prompt_question, prompt_context, parameters = default_parms, api_url=API_URL, headers=headers):
+    prompt = {
+        "question": prompt_question,
+        "context": prompt_context,
+    }
+
+    payload = {
+        "inputs": prompt,
+        "parameters": parameters
     }
     response = requests.post(api_url, headers=headers, json=payload)
     return response.json() #.json()[0]['generated_text']
@@ -37,6 +38,12 @@ context = "As of the most current census, Jacksonville, Florida has a population
 print("**** Roberta Model **** ")
 print(query(question, context))
 print("**** Bert Large Model **** ")
-print(query(question,context,"https://api-inference.huggingface.co/models/valhalla/bart-large-finetuned-squadv1"))
-print("**** Llama2 7b **** ")
-print(query(question,context,"https://api-inference.huggingface.co/models/cxllin/Llama2-7b-med-v1"))
+print(query(question,context, api_url="https://api-inference.huggingface.co/models/valhalla/bart-large-finetuned-squadv1"))
+print("**** GPT2 7b **** ")
+parms_v1 = { #Try and experiment with the parameters
+            "max_new_tokens": 250,
+            "temperature": 0.1,
+            "return_full_text": False,
+            "wait_for_model": True,
+        }
+print(query(question,context,parameters=parms_v1,api_url="https://api-inference.huggingface.co/models/gpt2"))
