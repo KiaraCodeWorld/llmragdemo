538ef2c2729d9b737cca92f39a2dda06cb9a836e
KiaraCodeWorld <128760279+KiaraCodeWorld@users.noreply.github.com>
2024-01-21 12:47:37 -0500

    Update app.py
    Modified function to take any Inference API URL, Questions / context combination ...

--- app.py
+++ app.py
@@ -1,31 +1,42 @@
 import os
 import requests
+import json
 
-API_TOKEN = os.environ["API_TOKEN"] #Set a API_TOKEN environment variable before running
-API_URL = "" #Add a URL for a model of your choosing
+# #Set a API_TOKEN environment variable before running
+API_TOKEN = os.environ["API_TOKEN"]
+API_URL =  "https://api-inference.huggingface.co/models/deepset/roberta-base-squad2" #"https://api-inference.huggingface.co/models/ESGBERT/EnvironmentalBERT-environmental" #Add a URL for a model of your choosing
 headers = {"Authorization": f"Bearer {API_TOKEN}"}
 
-def query(prompt):
+def query(prompt_question, prompt_context, api_url=API_URL, headers=headers):
+    prompt = {
+        "question": prompt_question,
+        "context": prompt_context,
+    }
+
     payload = {
         "inputs": prompt,
         "parameters": { #Try and experiment with the parameters
             "max_new_tokens": 1024,
-            "temperature": 0.6,
+            "temperature": 0.9,
             "top_p": 0.9,
             "do_sample": False,
-            "return_full_text": False
+            "return_full_text": False,
+            "wait_for_model": True,
         }
     }
-    response = requests.post(API_URL, headers=headers, json=payload)
-    return response.json()[0]['generated_text']
+    response = requests.post(api_url, headers=headers, json=payload)
+    return response.json() #.json()[0]['generated_text']
 
-question = "What is the population of Jacksonville, Florida?"
-context = "As of the most current census, Jacksonville, Florida has a population of 1 million."
-prompt = f"""Use the following context to answer the question at the end.
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print('***** LLM Demo - Inference API ***')
 
-{context}
+question = "What is the population of Miami, Florida?"
+context = "As of the most current census, Jacksonville, Florida has a population of 1.13 million."
 
-Question: {question}
-"""
-
-print(query(prompt))
+print("**** Roberta Model **** ")
+print(query(question, context))
+print("**** Bert Large Model **** ")
+print(query(question,context,"https://api-inference.huggingface.co/models/valhalla/bart-large-finetuned-squadv1"))
+print("**** Llama2 7b **** ")
+print(query(question,context,"https://api-inference.huggingface.co/models/cxllin/Llama2-7b-med-v1"))
